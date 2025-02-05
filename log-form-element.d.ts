02f9753e9f91a1ff3f52680b5130a9c16b7f5386
Nathan Knowler <nathan@knowler.dev>
2025-02-05 15:30:16 -0600

    Add docs to types

--- log-form-element.d.ts
+++ log-form-element.d.ts
@@ -1,3 +1,14 @@
+/**
+ * A custom element that logs form submissions to the console instead of its
+ * default action.
+ */
 export declare class LogFormElement extends HTMLElement {
+	/**
+	 * Registers the custom element in the `window`’s custom element registry as
+	 * `log-form` or the supplied tag name. Also, adds the class to the `window`.
+	 * This method is idempotent and will not attempt to register the element if
+	 * it’s already been done.
+	 * @param {string} [tagName="log-form"] - the tag name to register the custom element as.
+	 */
 	static define(tagName?: string): void;
 }
